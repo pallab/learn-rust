9b4d4c699e2669611bce9d36ecc26ab0b7d238e7
pallab <13886153+pallab@users.noreply.github.com>
2022-01-22 11:38:26 +0530

    Guessing game

---
+++ the-rust-book/3_guessing_game/src/main.rs
@@ -0,0 +1,37 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    
+    println!("Guess the number");
+
+    let secret = rand::thread_rng().gen_range(1..101);
+    let mut trials : u32 = 0;
+
+    loop {  
+        trials +=1;
+
+        println!("Enter your guess :");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess : u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("Bingo! You Win! Took you {} trials", trials);
+                break;
+            }
+        }
+    }
+}
