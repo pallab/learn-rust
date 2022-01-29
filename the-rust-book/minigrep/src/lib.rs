14911be2cde95208603a21779df8ed588f12a7c2
pallab <13886153+pallab@users.noreply.github.com>
2022-01-29 13:45:23 +0530

    refactor

--- the-rust-book/minigrep/src/lib.rs
+++ the-rust-book/minigrep/src/lib.rs
@@ -1,6 +1,7 @@
 use std::error::Error;
 use std::fs;
 use std::env;
+use std::env::Args;
 
 pub struct Config {
     pub query : String,
@@ -9,12 +10,18 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args : &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
-        let query  = args[1].clone();
-        let filename = args[2].clone();
+    pub fn new(mut args : env::Args) -> Result<Config, &'static str> {
+        args.next();
+
+        let query  = match args.next() {
+            Some(args) => args,
+            None => return Err("Did not get query")
+        };
+        let filename  = match args.next() {
+            Some(args) => args,
+            None => return Err("Did not get filename")
+        };
+
         let case_sensitive = env::var("CASE_SENSITIVE").is_ok();
 
         Ok(Config{ query, filename, case_sensitive })
@@ -38,23 +45,14 @@ pub fn run(config : Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'z>(query : &str, content : &'z str) -> Vec<&'z str> {
-    let mut results = vec![];
-    for line in content.lines() {
-        if line.contains(query){
-            results.push(line);
-        }
-    }
-    results
+    content
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 pub fn search_case_insensitive<'x>(query : &str, content : &'x str) -> Vec<&'x str> {
     let query = query.to_lowercase();
-    let mut results = vec![];
-    for line in content.lines() {
-        if line.to_lowercase().contains(&query){
-            results.push(line);
-        }
-    }
-    results
+    content.lines().filter(|line| line.to_lowercase().contains(query)).collect()
 }
 
 #[cfg(test)]
